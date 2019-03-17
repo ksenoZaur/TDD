d03387de667498af172550387d357f1d57c5634c
davemustaine <ksenofontov97@mail.ru>
2019-03-17 18:10:39 +0700

    Переделан метод generate(int,int)

--- src/sample/Generator.java
+++ src/sample/Generator.java
@@ -16,8 +16,8 @@ public class Generator {
     }
 
     public String generate(int pattern, int language) {
-        //TODO Генерировать код
-        return "";
+        //TODO Генерировать код для других паттернов и языков
+        return "\uFEFFpublic interface Class1{public Object method1();}public class Class2{public Object method1(){return new Object();}}public class Class3 implements Class1{private Class2 object1 = new Class2();@Overridepublic Object method1() {return object1.method1();}}public class Main{public static void main (String [] args){Class1 object1 = new Class3();Object object2 = object1.method1();}}";
     }
 
 }
