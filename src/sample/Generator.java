f07efd12b4474d39f63945768fb451b6b88853bc
davemustaine <ksenofontov97@mail.ru>
2019-03-17 16:55:12 +0700

    Добавлен метод generate(int, int) + TODO

--- src/sample/Generator.java
+++ src/sample/Generator.java
@@ -5,11 +5,16 @@ import java.util.Map;
 
 public class Generator {
     public Map<Integer, String> getPatterns() {
+        //TODO Добавить поддержку дрягих паттернов
         return  Map.of(0, "Adapter");
     }
 
     public Map<Integer, String> getLanguages() {
+        //TODO Добавить поддержку других языков
         return  Map.of(0, "Java");
     }
 
+    public void generate(int pattern, int language) {
+        //TODO Генерировать код
+    }
 }
